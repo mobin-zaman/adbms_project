e9bb12b8c7e0de3eccfec8788546c63c9131fab7
Sizan75 <sakiuzzaman26@gmail.com>
2019-11-30 16:04:17 +0600

    login done

---
+++ Front_End/admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+</head>
+<body>
+ <form>
+<div>
+	
+	<button type="Submit" id="fid">Faculty</button>
+	<br>
+	<button type="Submit" id="sid">Student</button>
+    <br>
+    <button type="Submit" id="pid">Project</button>
+    <br>
+    <button type="Submit" id="tid">Thesis</button>
+    <br>
+</div>
+</form>
+
+</body>
+</html>
