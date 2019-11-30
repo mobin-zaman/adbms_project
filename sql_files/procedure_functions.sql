8d22aaaca4c5f63cb38d7d46226e9d043eca1cd5
mobin-zaman <mobin_zaman@hotmail.com>
2019-11-30 19:03:33 +0600

    login function with exception handling done

--- sql_files/procedure_functions.sql
+++ sql_files/procedure_functions.sql
@@ -1,12 +1,19 @@
 CREATE OR REPLACE FUNCTION admin_login(u_name admin.username%type, passwd admin.password%type)
-return number(10) --return 0 if admin doesn't exist, else return the admin id
+return number --return 0 if admin doesn't exist, else return the admin id
 is
 admin_id NUMBER(10);
 BEGIN
     SELECT id INTO admin_id FROM admin WHERE USERNAME=u_name and PASSWORD = passwd;
-    if (id == null) then return 0;
-    end if;
-    return id;
+    RETURN admin_id;
+EXCEPTION
+    WHEN no_data_found THEN  --exception handling, bonus marks
+    RETURN 0;
+END;
+
+
+declare
+    admin_id number(10);
+begin
+    admin_id := admin_login('admins','admin');
+    DBMS_OUTPUT.PUT_LINE(admin_id);
 end;
-
-
