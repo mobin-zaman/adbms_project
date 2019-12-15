8442c3d343677eeef9a70f269d407c26b426c56e
mobin-zaman <mobinzamandev@gmail.com>
2019-12-15 10:14:28 +0600

    api url changed

--- backend/app.py
+++ backend/app.py
@@ -34,4 +34,4 @@ def shutdown():
     os.system('shutdown now')
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
--- backend/database/faculty_methods.py
+++ backend/database/faculty_methods.py
@@ -17,11 +17,25 @@ def insert_faculty_procedure(id, name, email, phone_number, did):
             return error_message(error_obj.message)
 
 
+def update_faculty_procedure(id, name, email, phone_number, did):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        try:
+            cursor.callproc('faculty_package.update_faculty', [id, name, email, phone_number, did])
+            conn.commit()
+            return "success"
+        except DatabaseError as err:
+            error_obj, = err.args
+            print("error CODE: ", error_obj.code)
+            print("error MESSAGE: ", error_obj.message.split("\n")[0])
+            return error_message(error_obj.message)
+
+
 def search_faculty(name):
     conn = get_connection()
 
     with conn.cursor() as cursor:
         cursor.execute('select * from faculty where upper(name) like upper(:name)', {
-            'name': '%' + name + '%'})  # needed for capitalizing names, or else search doesn't weork
+            'name': '%' + name + '%'})  # needed for capitalizing names, or else search doesn't work
         result = dict_mapper(cursor)
         return result
